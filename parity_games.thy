1f57e1e954299855cdc59ac71c169d2c2aafb402
Christoph Dittmann <github@christoph-d.de>
2015-05-30 16:58:09 +0200

    parity games

--- parity_games.thy
+++ parity_games.thy
@@ -43,49 +43,49 @@ type_synonym Path = "nat \<Rightarrow> Vertex option"
 type_synonym Strategy = "Vertex \<Rightarrow> Vertex option"
 
 definition (in Digraph) deadend :: "Vertex \<Rightarrow> bool" where
-  "deadend v == \<not>(\<exists>w \<in> V. (v,w) \<in> E)"
+  [simp]: "deadend v \<equiv> \<not>(\<exists>w \<in> V. (v,w) \<in> E)"
 
 definition (in Digraph) infinite_path :: "Path \<Rightarrow> bool" where
-  "infinite_path P == \<forall>i. P i  \<noteq> None"
+  [simp]: "infinite_path P \<equiv> \<forall>i. P i  \<noteq> None"
 
 definition (in Digraph) finite_path :: "Path \<Rightarrow> bool" where
-  "finite_path P == \<exists>i. \<forall>j. (j > i \<longleftrightarrow> P j = None)"
+  [simp]: "finite_path P \<equiv> \<exists>i. \<forall>j. (j > i \<longleftrightarrow> P j = None)"
 
 definition (in Digraph) path_dom :: "Path \<Rightarrow> nat set" where
-  "path_dom P = {i. P i \<noteq> None}"
+  [simp]: "path_dom P = {i. P i \<noteq> None}"
 
-definition (in Digraph) valid_path :: "Path \<Rightarrow> bool" where
-  "valid_path P == (infinite_path P \<or> finite_path P)
+definition  (in Digraph) valid_path :: "Path \<Rightarrow> bool" where
+  [simp]: "valid_path P \<equiv> (infinite_path P \<or> finite_path P)
     \<and> (\<forall>i \<in> path_dom P.
       the (P i) \<in> V
       \<and> (\<not>deadend (the (P i)) \<longrightarrow> (the (P i), the (P (i+1))) \<in> E))"
 
 lemma (in Digraph) paths_are_contiguous:
   assumes "valid_path P"
-  shows "P i \<noteq> None \<Longrightarrow> \<forall>j < i. P j \<noteq> None"
-  by (metis assms finite_path_def infinite_path_def le_less_trans less_imp_le valid_path_def)
+  shows "P i \<noteq> None \<Longrightarrow> \<forall>j \<le> i. P j \<noteq> None"
+  by (metis assms dual_order.order_iff_strict dual_order.strict_trans finite_path_def infinite_path_def valid_path_def)
 
 lemma (in Digraph) path_dom_ends_on_finite_paths:
-  assumes "valid_path P" "finite_path P"
+  assumes finite: "finite_path P"
   shows "\<exists>!i \<in> path_dom P. P (i + 1) = None"
   proof -
-    obtain i where i_def: "i \<in> path_dom P \<and> P (i + 1) = None"
-      by (metis (mono_tags, lifting) Suc_eq_plus1 assms(2) finite_path_def less_Suc_eq less_irrefl mem_Collect_eq path_dom_def)
+    obtain i where "\<forall>j. (j > i \<longleftrightarrow> P j = None)" using finite by fastforce
+    hence "i \<in> path_dom P \<and> P (i + 1) = None" by auto
     thus ?thesis
-      by (metis (mono_tags, lifting) One_nat_def add.right_neutral add_Suc_right assms(2) finite_path_def less_Suc_eq mem_Collect_eq path_dom_def)
+      by (metis (mono_tags, lifting) One_nat_def add.right_neutral add_Suc_right finite finite_path_def less_Suc_eq mem_Collect_eq path_dom_def)
   qed
 
 (* The set of nodes that occur infinitely often on a given path. *)
 definition (in ParityGame) path_inf :: "Path \<Rightarrow> Vertex set" where
-  "path_inf P == {v. (\<exists>i. P i = Some v) \<and> (\<forall>i. P i = Some v \<longrightarrow> (\<exists>j > i. P j = Some v))}"
+  "path_inf P \<equiv> {v. (\<exists>i. P i = Some v) \<and> (\<forall>i. P i = Some v \<longrightarrow> (\<exists>j > i. P j = Some v))}"
 
 (* The set of priorities that occur infinitely often on a given path. *)
 definition (in ParityGame) path_inf_priorities :: "Path \<Rightarrow> nat set" where
-  "path_inf_priorities P == {i. \<exists>v \<in> path_inf P. i = priority v}"
+  "path_inf_priorities P \<equiv> {i. \<exists>v \<in> path_inf P. i = priority v}"
 
 (* True iff \<sigma> is a positional strategy for the given player. *)
 definition (in ParityGame) positional_strategy :: "Player \<Rightarrow> Strategy \<Rightarrow> bool" where
-  "positional_strategy p \<sigma> == \<forall>v \<in> VV p. \<not>deadend v \<longrightarrow> \<sigma> v \<noteq> None"
+  "positional_strategy p \<sigma> \<equiv> \<forall>v \<in> VV p. \<not>deadend v \<longrightarrow> \<sigma> v \<noteq> None"
 
 definition (in ParityGame) path_conforms_with_strategy :: "Player \<Rightarrow> Path \<Rightarrow> Strategy \<Rightarrow> bool" where
   "path_conforms_with_strategy p P \<sigma> = (\<forall>i \<in> path_dom P. the (P i) \<in> VV p \<longrightarrow> \<sigma>(the (P i)) = P (i+1))"
@@ -94,7 +94,7 @@ lemma (in "ParityGame") path_inf_is_nonempty:
   assumes "valid_path P" "infinite_path P"
   shows "\<exists>v. v \<in> path_inf P"
   proof -
-    from assms have "\<forall>i. the (P i) \<in> V" by (simp add: infinite_path_def path_dom_def valid_path_def)
+    from assms have "\<forall>i. the (P i) \<in> V" by simp
     hence "\<exists>v \<in> V. (\<exists>i. the (P i) = v) \<and> (\<forall>i. (the (P i) = v \<longrightarrow> (\<exists>j > i. the (P j) = v)))"
       using pigeon_hole_principle[of "V" "\<lambda>i. the (P i)"] finite_vertex_set by blast
     hence "\<exists>v \<in> V. (\<exists>i. P i = Some v) \<and> (\<forall>i. (P i = Some v \<longrightarrow> (\<exists>j > i. P j = Some v)))" by (metis assms(2) infinite_path_def option.collapse)
@@ -104,7 +104,7 @@ lemma (in "ParityGame") path_inf_is_nonempty:
 lemma (in "ParityGame") path_inf_priorities_is_nonempty:
   assumes "valid_path P" "infinite_path P"
   shows "\<exists>a. a \<in> path_inf_priorities P"
-  using assms path_inf_is_nonempty path_inf_priorities_def by fastforce
+  using assms path_inf_is_nonempty[of P] path_inf_priorities_def by auto
 
 lemma (in "ParityGame") path_inf_priorities_has_minimum:
   assumes "valid_path P" "infinite_path P"
@@ -126,7 +126,7 @@ lemma winning_priority_for_one_player:
 
 (* True iff the path is winning for the given player. *)
 definition (in ParityGame) winning_path :: "Player \<Rightarrow> Path \<Rightarrow> bool" where
-  "winning_path p P ==
+  [simp]: "winning_path p P \<equiv>
     (infinite_path P \<and> (\<exists>a \<in> path_inf_priorities P. (\<forall>b \<in> path_inf_priorities P. a \<le> b) \<and> winning_priority p a))
     \<or> (finite_path P \<and> (\<exists>i \<in> path_dom P. P (i+1) = None \<and> the (P i) \<in> VV p**))"
 
@@ -142,9 +142,10 @@ lemma (in "ParityGame") paths_are_winning_for_exactly_one_player:
     hence finite: "finite_path P" using assms valid_path_def by blast
     then obtain i where i_def: "i \<in> path_dom P \<and> P (i+1) = None" using assms path_dom_ends_on_finite_paths by blast
     def v \<equiv> "the (P i)" (* the last vertex in the path *)
+    have "\<And>q. winning_path q P \<longleftrightarrow> (\<exists>i \<in> path_dom P. P (i+1) = None \<and> the (P i) \<in> VV q**)"
+      using not_infinite finite winning_path_def by blast
     hence "\<And>q. winning_path q P \<longleftrightarrow> v \<in> VV q**"
-      using not_infinite assms local.finite path_dom_ends_on_finite_paths winning_path_def i_def by auto
-    hence "\<And>q. winning_path q P \<equiv> v \<in> VV q**" by fastforce
+      using not_infinite finite path_dom_ends_on_finite_paths i_def v_def by blast
     hence "v \<in> VV p** \<longleftrightarrow> \<not>v \<in> VV p \<Longrightarrow> ?thesis"
       by (metis (full_types) Player.exhaust other_player.simps(1) other_player.simps(2))
     thus ?thesis
@@ -157,7 +158,7 @@ lemma (in "ParityGame") paths_are_winning_for_one_player:
   by (metis (full_types) VV.elims assms paths_are_winning_for_exactly_one_player)
 
 definition (in ParityGame) winning_strategy :: "Player \<Rightarrow> Strategy \<Rightarrow> Vertex \<Rightarrow> bool" where
-  "winning_strategy p \<sigma> v == \<forall>P. P 0 = Some v \<longrightarrow> path_conforms_with_strategy p P \<sigma> \<longrightarrow> winning_path p P"
+  "winning_strategy p \<sigma> v \<equiv> \<forall>P. P 0 = Some v \<longrightarrow> path_conforms_with_strategy p P \<sigma> \<longrightarrow> winning_path p P"
 
 theorem (in "ParityGame") positional_strategy_exists:
   assumes "v \<in> V"
