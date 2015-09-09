75d92c95c761f3be79759ed0e41eb79a22649894
Christoph Dittmann <github@christoph-d.de>
2015-09-09 12:30:05 +0200

    Show llength_ltake'

--- parity_game.thy
+++ parity_game.thy
@@ -86,6 +86,9 @@ next
   qed
 qed
 
+lemma llength_ltake': "enat n < llength xs \<Longrightarrow> llength (ltake (enat n) xs) = enat n"
+  by (metis assms llength_ltake min.strict_order_iff)
+
 (* 'a is the vertex type. *)
 type_synonym 'a Edge = "'a \<times> 'a"
 (* A path is a possibly infinite list of vertices. *)
