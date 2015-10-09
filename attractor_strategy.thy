d42540ab4811c829b1c42f441913d418ef5602f4
Christoph Dittmann <github@christoph-d.de>
2015-10-09 17:45:59 +0200

    Simplify strategy_attracts_extends_VVp

--- attractor_strategy.thy
+++ attractor_strategy.thy
@@ -28,65 +28,24 @@ proof-
       hence "vmc_path G P'' w p ?\<sigma>" using vmc_path_ltl by (simp add: w0_def)
       then interpret vmc_path G P'' w p ?\<sigma> .
 
-      have "\<exists>n. enat n < llength P \<and> P $ n \<in> W \<and> lset (ltake (enat n) P) \<subseteq> insert v0 S" proof (cases)
-        assume "v0 \<in> lset P'' \<and> ?\<sigma> v0 \<noteq> \<sigma> v0"
+      have *: "v0 \<notin> S - W" using `v0 \<notin> S` by blast
+      have "override_on (\<sigma>(v0 := w)) \<sigma> (S - W) = ?\<sigma>"
+        by (rule ext) (metis * fun_upd_def override_on_def)
+      hence "strategy_attracts p ?\<sigma> S W"
+        using strategy_attracts_irrelevant_override[OF \<sigma>(2,1) `strategy p ?\<sigma>`] by simp
+      hence "strategy_attracts_via p ?\<sigma> w S W" unfolding strategy_attracts_def using `w \<in> S` by blast
+      then obtain n where n: "enat n < llength P''" "P'' $ n \<in> W" "lset (ltake (enat n) P'') \<subseteq> S"
+        unfolding strategy_attracts_via_def using vmc_path by blast
 
-        with \<sigma>(1) `strategy p ?\<sigma>` `v0 \<in> VV p` `\<not>deadend v0`
-          obtain P' n where
-            "vmc_path G P' w p \<sigma>"
-            and n_valid: "enat (Suc n) < llength P'" "enat (Suc n) < llength P''"
-            and P'_P''_same_prefix: "ltake (enat (Suc n)) P' = ltake (enat (Suc n)) P''"
-            and P''_n: "P'' $ n \<in> VV p" "\<not>deadend (P'' $ n)" "?\<sigma> (P'' $ n) \<noteq> \<sigma> (P'' $ n)"
-          using path_conforms_with_strategy_update_path by blast
-        then interpret vmc_path G P' w p \<sigma> by blast
-
-        have "P'' $ n = v0" using P''_n(3) by (meson fun_upd_apply)
-        have "P' $ 0 = w" by simp
-
-        obtain m where m: "enat m < llength P'" "P' $ m \<in> W" "lset (ltake (enat m) P') \<subseteq> S"
-          using `strategy_attracts_via p \<sigma> w S W` strategy_attracts_viaE by blast
-
-        have "m \<le> n" proof (rule ccontr)
-          assume "\<not>m \<le> n"
-          hence "Suc n \<le> m" by presburger
-          hence "enat (Suc n) \<le> enat m" by simp
-          with m(3) have "lset (ltake (enat (Suc n)) P') \<subseteq> S" by (meson lprefix_lset' order_trans)
-          with P'_P''_same_prefix have *: "lset (ltake (enat (Suc n)) P'') \<subseteq> S" by simp
-          with n_valid(2) have "enat n < llength P''" using Suc_ile_eq le_less by blast
-          hence "enat n < llength (ltake (enat (Suc n)) P'')" by simp
-          with * have "P'' $ n \<in> S"
-            using lset_lnth[of "ltake (enat (Suc n)) P''" S n]
-            by (metis (no_types) lprefix_lnthD ltake_is_lprefix)
-          with `P'' $ n = v0` `v0 \<notin> S` show False by blast
-        qed
-        with P'_P''_same_prefix have "P' $ m = P'' $ m"
-          using ltake_lnth[of "enat (Suc n)" P' P'' m] by simp
-        with m(2) have "P'' $ m \<in> W" by simp
-        hence 1: "P $ Suc m \<in> W" by (simp add: lnth_ltl)
-
-        from P'_P''_same_prefix `m \<le> n` m(3)
-          have "lset (ltake (enat m) P'') \<subseteq> S"
-          using ltake_eq_ltake_antimono by fastforce
-        hence 2: "lset (ltake (enat (Suc m)) P) \<subseteq> insert v0 S"
-          unfolding P''_def using lset_ltake_Suc'[of P] by simp
-
-        from `m \<le> n` n_valid(2) have "enat (Suc m) < llength P''"
-          by (metis Suc_ile_eq dual_order.strict_iff_order dual_order.strict_trans enat_ord_simps(2))
-        moreover have "llength P'' \<le> llength P"
-          unfolding P''_def by (metis Coinductive_List.ltl_ldrop eq_refl lnull_ldrop lnull_ltlI)
-        ultimately have 3: "enat (Suc m) < llength P" by simp
-
-        with 1 2 3 show ?thesis by blast
-      next
-        assume "\<not>(v0 \<in> lset P'' \<and> ?\<sigma> v0 \<noteq> \<sigma> v0)"
-        then interpret vmc_path G P'' w p \<sigma>
-          using path_conforms_with_strategy_irrelevant'[of p P'' \<sigma> v0 w] P_conforms P_v0
-          by unfold_locales fastforce
-        have "\<exists>n. enat n < llength P'' \<and> P'' $ n \<in> W \<and> lset (ltake (enat n) P'') \<subseteq> S"
-          using strategy_attracts_viaE[OF `strategy_attracts_via p \<sigma> w S W`] by metis
-        thus ?thesis
-          by (metis P''_def P_LCons' enat_ltl_Suc llist.disc(2) lnth_0 lnth_ltl lset_ltake_Suc')
+      have "enat (Suc n) < llength P" using P''_def enat_ltl_Suc n(1) by blast
+      moreover have "P $ Suc n \<in> W" using P''_def n(2) by (metis P_LCons' Ptl_LCons lnth_Suc_LCons)
+      moreover have "lset (ltake (enat (Suc n)) P) \<subseteq> insert v0 S" proof-
+        have "ltake (eSuc (enat n)) P = LCons v0 (ltake (enat n) P'')"
+          unfolding P''_def by (metis P_LCons' Ptl_LCons ltake_eSuc_LCons)
+        thus ?thesis using n(3) eSuc_enat by auto
       qed
+      ultimately have "\<exists>n. enat n < llength P \<and> P $ n \<in> W \<and> lset (ltake (enat n) P) \<subseteq> insert v0 S"
+        by blast
     }
     thus ?thesis unfolding strategy_attracts_via_def by blast
   qed
